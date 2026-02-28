8c57b844bc911c784f8ddc709b2bf2d39677b672
jb <vagabigben@tutanota.com>
2026-02-28 18:27:01 +0000

    Added more GUI functions and vault logic changes
    Added functions for the main menu. Changed some logic in the vault

--- pwManager.py
+++ pwManager.py
@@ -2,6 +2,7 @@ import base64
 import os
 import tkinter as tk
 from tkinter import filedialog as fd
+from tkinter import messagebox
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
@@ -65,19 +66,16 @@ class PasswordManager:
                     self.vaultDictionary[website] = Fernet(self.vaultKey).decrypt(password.encode()).decode()
         except Exception as e:
             print(e)
+            
     def newEntry(self,website:str,password:str)-> None:
         self.vaultDictionary[website] = password
-
-        if self.vaultFile is not None:
-            with open(self.vaultFile,'a') as f:
-                encryptedPw = Fernet(self.vaultKey).encrypt(password.encode())
-                f.write(website + ":" + encryptedPw.decode() + "\n")
 
     def deleteEntry(self, website:str) -> None:
         if website in self.vaultDictionary:
             self.vaultDictionary.pop(website) 
+
     
-    def saveToFile(self):
+    def saveToFile(self) -> None:
         if self.vaultFile is not None:
             try:
                 with open(self.vaultFile, "w") as f:
@@ -86,9 +84,8 @@ class PasswordManager:
                         f.write(site + ":" + encryptedPassword.decode() + "\n")
             except Exception as e:
                 print(e)
-
-    def getPassword(self,website:str) -> str:
-        return self.vaultDictionary[website]
+    def getPasswords(self) -> dict:
+        return self.vaultDictionary
     
     def importPasswords(self, passwords:dict) -> None:
         for website, password in passwords.items():
@@ -107,7 +104,8 @@ class ProgramGUI:
         self.vaultPath = None
         self.vaultKeyPath = None
         self.defaultTitleFont = ("Arial", 16, "Bold") # annoying to keep typing this so decided to put it here
-        self.displayLoginMenu()
+        self.root.protocol(("WM_DELETE_WINDOW"), self.exit)
+        self.displayStartupMenu()
 
     def displayStartupMenu(self):
         # function called on initialization
@@ -116,26 +114,37 @@ class ProgramGUI:
         label = tk.Label(self.root, text="Welcome! Choose an option below:", font=self.defaultTitleFont)
         label.pack(pady=15)
 
-        tk.Button(self.root, text="Load Vault", width=20,command=self.loadVaultLocation).pack(pady=10)
+        tk.Button(self.root, text="Load Vault", width=20,command=self.loadVault).pack(pady=10)
         tk.Button(self.root, text="Create new vault", width=20, command=self.createVault).pack(pady=10)
         
 
-    def loadVaultLocation(self):
+    def loadVault(self):
         # need to add a file cancel check
         keyFile = fd.askopenfilename(title="Select your key file", filetypes=[("Key files", "*.key")])
+        if not keyFile:
+            return
         vaultFile = fd.askopenfilename(title="Select your vault file", filetypes=[("Text files", "*.txt")])
+        if not vaultFile:
+            return
         self.vaultKeyPath = keyFile
         self.vaultPath = vaultFile
+        self.displayLoginMenu()
 
     def createVault(self):
         # need to add a file cancel check
-        vaultFile = fd.askdirectory(title = "Select where you want to create your vault")
-        
+        vaultFile = fd.asksaveasfilename(title = "Select where you want to create your vault")
+        if not vaultFile:
+            return
         self.vaultPath = vaultFile
 
-        vaultKeyFile = fd.askdirectory(title="Where do you want to store your keyfile?")
+        vaultKeyFile = fd.asksaveasfilename(title="Where do you want to store your keyfile?")
+        if not vaultKeyFile:
+            return
         self.vaultKeyPath = vaultKeyFile
-        
+
+
+    def createNewMasterPassword(self):
+        pass
 
     def clearGUI(self):
         # used to clear all elements within the GUI. Used to prevent visual bugs navigating between menus
@@ -146,16 +155,15 @@ class ProgramGUI:
     def displayLoginMenu(self):
         self.clearGUI()
         label = tk.Label(self.root, text="Enter your master password",font=self.defaultTitleFont)
-        label.pack()
+        label.pack(pady=5)
         pwEntry = tk.Entry(self.root, width=30)
         pwEntry.pack()
 
-        
         feedbackLabel = tk.Label(self.root,text="", width=20)
-        feedbackLabel.pack()
+        feedbackLabel.pack(pady=5)
 
-        loginButton = tk.Button(self.root, text = "Login", command=self.tryLogin, width=20)
-        loginButton.pack()
+        loginButton = tk.Button(self.root, text = "Login", command=tryLogin, width=20)
+        loginButton.pack(pady=5)
 
         # looked up different ways to do this and decided to do it this way. Can't pass arguments with tk button.
         def tryLogin():
@@ -177,9 +185,90 @@ class ProgramGUI:
         label = tk.Label(self.root, text="Main Menu", font=self.defaultTitleFont)
         label.pack(pady=10)
 
-        addPwButton = tk.Button(self.root, text="Add Password", width=20, command=self.addPassword).pack(pady=5)
-        viewPwButton = tk.Button(self.root, text="View Passwords", width=20, command=self.viewPassword).pack(pady=5)
-        deletePwButton = tk.Button(self.root, text="Delete Password", width=20, command=self.deletePassword).pack(pady=5)
-        exitButton = tk.Button(self.root, text="Exit", width=20, command = self.root.destroy).pack(pady=5)
+        addPwButton = tk.Button(self.root, text="Add Password", width=20, command=self.addPassword)
+        addPwButton.pack(pady=5)
+        viewPwButton = tk.Button(self.root, text="View Passwords", width=20, command=self.viewPasswords)
+        viewPwButton.pack(pady=5)
+        deletePwButton = tk.Button(self.root, text="Delete Password", width=20, command=self.deletePassword)
+        deletePwButton.pack(pady=5)
+        exitButton = tk.Button(self.root, text="Exit", width=20, command = self.exit)
+        exitButton.pack(pady=5)
+
+    
+    def exit(self):
+        if messagebox.askyesno(title="Are you sure you want to exit? (Changes will be saved on exit)"):
+            self.passwordManager.saveToFile()
+            self.root.destroy()
       
-       
+    def addPassword(self):
+        self.clearGUI()
+        topFrame = tk.Frame(self.root)
+        topFrame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+
+        backButton = tk.Button(topFrame,text="Back to main menu", command=self.displayMainMenu, width=10)
+        backButton.pack(side=tk.LEFT)
+
+        websiteLabel = tk.Label(self.root, text="Enter a website: ")
+        websiteLabel.pack(pady=5)
+        websiteEntry = tk.Entry(self.root, width=30)
+        websiteEntry.pack()
+
+        passwordLabel = tk.Label(self.root, text="Enter the password")
+        passwordLabel.pack(pady=5)
+        passwordEntry = tk.Entry(self.root, width=30)
+        passwordEntry.pack()
+
+        feedbackLabel = tk.Label(self.root,text="",width=20)
+        feedbackLabel.pack(pady=5)
+        submitButton = tk.Button(self.root, text="Add to vault", command=addToVault, width=20)
+        submitButton.pack(pady=5)
+
+        def addToVault():
+            if not websiteEntry.get().strip() or not passwordEntry.get().strip():
+                feedbackLabel.config(text="Invalid Entry! Website or password is empty!")
+            else:
+                self.passwordManager.newEntry(websiteEntry.get().strip(),passwordEntry.get().strip())
+                feedbackLabel.config(text="Added password successfully!")
+
+    def viewPasswords(self):
+        self.clearGUI()
+
+        topFrame = tk.Frame(self.root)
+        topFrame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+
+        backButton = tk.Button(topFrame,text="Back to main menu", command=self.displayMainMenu, width=10)
+        backButton.pack(side=tk.LEFT)
+
+        contents = ""
+        for website, password in self.passwordManager.getPasswords().items():
+            contents += f"{website}: {password}\n"
+
+        displayLabel = tk.Label(self.root,text=contents, width=50)
+        displayLabel.pack()
+    
+    def deletePassword(self):
+        self.clearGUI()
+        topFrame = tk.Frame(self.root)
+        topFrame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+
+        backButton = tk.Button(topFrame,text="Back to main menu", command=self.displayMainMenu, width=10)
+        backButton.pack(side=tk.LEFT)
+
+        websiteLabel = tk.Label(self.root, text="Enter the website")
+        websiteLabel.pack(pady=5)
+        websiteEntry = tk.Entry(self.root, width=30)
+        websiteEntry.pack()
+
+        feedbackLabel = tk.Label(self.root,text="",width=20)
+        feedbackLabel.pack(pady=5)
+        submitButton = tk.Button(self.root, text="Delete from vault",command=deleteFromVault, width=20)
+        submitButton.pack(pady=5)
+
+        def deleteFromVault():
+            if not websiteEntry.get().strip():
+                feedbackLabel.config(text="Entry can't be empty!")
+            elif websiteEntry.get().strip() not in self.passwordManager.getPasswords().keys():
+                feedbackLabel.config(text="Not in the vault!")
+            else:
+                self.passwordManager.deleteEntry(websiteEntry.get().strip())
+
